1fbe34343aa38fac3ed2dba69fde32f6ebe03218
tejagadepalli <97437250+tejagadepalli@users.noreply.github.com>
2022-05-18 11:59:51 +0530

    num=int(input()) sqr=num*num sumofdigit=0 while sqr>0:     sumofdigit=sumofdigit+sqr%10     sqr=sqr//10 if(num==sumofdigit):     print ("Neon Number") else:     print("Not Neon Number")

---
+++ Neon_Number.py
@@ -0,0 +1,10 @@
+num=int(input())
+sqr=num*num
+sumofdigit=0
+while sqr>0:
+    sumofdigit=sumofdigit+sqr%10
+    sqr=sqr//10
+if(num==sumofdigit):
+    print ("Neon Number")
+else:
+    print("Not Neon Number")
